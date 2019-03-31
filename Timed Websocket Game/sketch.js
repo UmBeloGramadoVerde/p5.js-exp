7ee0aa975134304009749111c7b281f6d900abe6
UmBeloGramadoVerde <kaynarodrigues@live.com>
2019-03-31 17:04:55 +0100

    Update sketch.js

--- Timed Websocket Game/sketch.js
+++ Timed Websocket Game/sketch.js
@@ -1,27 +1,26 @@
-//The app now can generate the ball and make it be contained inside the arena (fixes need for inside the arena, ocasional crashes)
-//Implemente the "boost" method in order to implement the rithym on the next phase
-//Functions to detect distance and push implemented for interacting with all kinds os arenas and players
-
 let width = 400;
 let height = 400;
+let MAX = 400;
+let MIN = 10;
+
 
 function setup() {
 
   createCanvas(width, height);
-  terrain = new Terrain(10, 400);
+  terrain = new Terrain(MIN, MAX);
   terrain.setup();
-  ball = new Ball(50,50,20);
+  ball = new Ball(MAX/2,MAX/2,20);
 }
 
 function draw() {
 
   translate(width/2, height/2);
   translate(-ball.pos.x, -ball.pos.y);
-  background(51);
+  background(92,39,81);
   ball.move();
   terrain.draw();
   ball.draw();
-  let closest = ball.checkBoundaries(terrain);
+  ball.checkBoundaries(terrain);
   ball.keepInside(terrain);
 }
 
@@ -31,5 +30,5 @@ function mousePressed() {
   console.log("Ball X: "+ ball.pos.x+" Ball Y:"+ball.pos.y);
   console.log("Mouse X: "+ mouse.x+"Mouse Y: "+mouse.y);
   v = p5.Vector.sub(mouse, ball.pos);
-  ball.burst(mouse);
+  ball.boost(mouse);
 }
